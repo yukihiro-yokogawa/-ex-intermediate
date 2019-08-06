ed00fe342299eaaaeb2fa97a55c09718a1196191
yukihiro-yokogawa <yukihiro.yokogawa@rakus-partners.co.jp>
2019-08-06 10:34:18 +0900

    リポジトリクラスに野球―チームの詳細情報を一件検索するloadメソッドを作成しました。

--- src/main/java/com/example2/Repository/BaseballRepository.java
+++ src/main/java/com/example2/Repository/BaseballRepository.java
@@ -4,11 +4,19 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 import com.example2.Domein.BaseballTeam;
 
+/**
+ * 野球チームのデータベースを操作するクラスです.
+ * 
+ * @author yukihiro.yokogawa
+ *
+ */
 @Repository
 public class BaseballRepository {
 
@@ -26,12 +34,33 @@ public class BaseballRepository {
 		return baseballteam;
 	};
 
+	/**
+	 * 全件検索のメソッドです.
+	 * 
+	 * @return チームリスト
+	 */
 	public List<BaseballTeam> findByAll() {
-		String sql = "SELECT league_name,team_name,headquaters,inauguation,history FROM teams WHERE id=:id ORDER BY inauguation";
-		
+		String sql = "SELECT league_name FROM teams ORDER BY inauguation";
+
 		List<BaseballTeam> teamList = template.query(sql, BASEBALLTEAM_ROW_MAPPER);
-		
+
 		return teamList;
+	}
+
+	/**
+	 * 野球チームの詳細情報を一軒検索するメソッドです.
+	 * 
+	 * @param id 球団id
+	 * @return 野球チームの詳細情報
+	 */
+	public BaseballTeam load(Integer id) {
+		String sql = "SELECT league_name,team_name,headquaters,inauguation,history FROM teams WHERE id=:id";
+
+		SqlParameterSource param = new MapSqlParameterSource().addValue("id", id);
+
+		BaseballTeam baseballteam = template.queryForObject(sql, param, BASEBALLTEAM_ROW_MAPPER);
+
+		return baseballteam;
 	}
 
 }
